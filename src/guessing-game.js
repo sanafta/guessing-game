c3f5a9e13bfd57f01bbbca293c5232e65a0aa2db
Ekaterina Kornienko <sanafta.2011@gmail.com>
2022-08-16 17:02:05 +0300

    task copmlete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+	constructor() { }
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
 
-    setRange(min, max) {
+	guess() {
+		this.number = Math.ceil((this.min + this.max) / 2);
+		return this.number;
+	}
 
-    }
+	lower() {
+		this.max = this.number;
+	}
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = this.number;
+	}
 }
 
 module.exports = GuessingGame;
